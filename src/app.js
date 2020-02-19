1d40e86d94e064a83d7d54e4b4030f134d868c81
souru98 <kulkarnisourabh1998@gmail.com>
2020-02-19 22:00:40 +0530

    edited

--- src/app.js
+++ src/app.js
@@ -20,32 +20,60 @@ const PLAYERS = [
     "Vader",
     "Slingo"
 ];
+let detailedPlayers = [];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = [];
+    for (var i = 0; i < PLAYERS.length; i++) {
+        var players = {
+            name: PLAYER[i],
+            strength: getRandomStrength(),
+            image: "./images/super-" + (i + 1) + ".png",
+            type: "hero|villan"
+        }
+        detailedPlayer.push(players);
+        console.log(detailedplayers);
+    }
     // Create players using for loop
     // Type your code here
 
-    return detailedPlayers;
+    // Where is villain? One image goes to hero and other image goes as villain?
+    //Use a ternary operator to push even numbers to hero and odd numbers to hero
+    player = i % 2 == 0 ? 'true' : 'false';
 }
+return detailedPlayers;
+
 
 // getting random strength
+//https://www.w3schools.com/jsref/jsref_random.asp - check this to generate random numbers
+
+
 const getRandomStrength = () => {
     // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+    // Note: You can use Math.randm() and Math.ceil()
+    let random = Math.floor((Math.random() * 10) + 1);
+    return random;
 }
 
 const buildPlayers = (players, type) => {
-    let fragment = '';
+        // Loop through players and accumulate HTML template
+        // depending of type of player(hero|villain)
+        // Type your code here
+        var fragment = '';
+        for (var i = 0; i < PLAYERS.length; i++) {
+            fragment = `
+            <div class="player">
+            <img src="${detailedPlayers[i].image}" alt = "characters">
+            <h4> ${detailedPlayers[i].name}</h4>
+            <h1>${detailedPlayers[i].strength}</h1>
+            </div>
+             `;
 
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
-    return fragment;
-}
-// Display players in HTML
+            //Where did you add the alternate name for the image? (Note: Use alt inside the image tag)
+        }
+        return fragment;
+    }
+    // Display players in HTML
 const viewPlayers = (players) => {
 
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
